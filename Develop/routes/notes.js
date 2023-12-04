b6d479506fcd4f819655c0f15c5ea296a48463ba
Nate McMahon <nathaniel.mcmahon@hrblock.com>
2023-12-03 23:04:44 -0600

    set up server.js file and am able to host at localhost:3001

---
+++ Develop/routes/notes.js
@@ -0,0 +1,32 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+// GET Route for retrieving all the tips
+notes.get('/', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
+
+notes.post('/', (req, res) => {
+    console.info(`${req.method} request received to add a notes`);
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+        title,
+        text,
+        note_id: uuid(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully`);
+    } else {
+        res.error('Error in adding note');
+    }
+});
+  
+  module.exports = notes;
+  
